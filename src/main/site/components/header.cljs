d1e651298d785707583bc9f278b73f5cdfeb6f5d
Anthony Fuentes <anthony@anthonyfuentes.us>
2020-08-19 14:34:04 -0700

    Implement header component

--- src/main/site/components/header.cljs
+++ src/main/site/components/header.cljs
@@ -1,4 +1,7 @@
-(ns site.components.header)
+(ns site.components.header
+  (:require [site.components.contact-info :as ci]))
 
-(defn render []
-  [:header.header "header"])
+(defn render [props]
+  [:header.header
+   [:h1 (:name props)]
+   [ci/render (:contact-info props)]])
--- src/test/site/components/header_test.cljs
+++ src/test/site/components/header_test.cljs
@@ -1,10 +1,18 @@
 (ns site.components.header-test
   (:require [cljs.test :refer [deftest is]]
-            [site.enzyme :refer [shallow]]
+            [site.enzyme :as e :refer [shallow]]
             [site.components.header :refer [render]]))
 
 (deftest has-class-header
-  (-> (render)
+  (-> (render {})
       shallow
       (.hasClass "header")
+      is))
+
+(deftest renders-contact-component-info
+  (-> (render {})
+      e/render
+      (.find ".contact-info")
+      (.-length)
+      (= 1)
       is))
