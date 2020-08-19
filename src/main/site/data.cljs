8fff675fff52b96329fb2af2f0660ee46d2ac0cc
Anthony Fuentes <anthony@anthonyfuentes.us>
2020-08-19 14:44:57 -0700

    Pass resume data to components

---
+++ src/main/site/data.cljs
@@ -0,0 +1,22 @@
+(ns site.data)
+
+(def github
+  {:display "anthonyfuentes"
+   :url "https://github.com/anthonyfuentes"})
+
+(def linkedin
+  {:display "anthonycfuentes"
+   :url "https://www.linkedin.com/in/anthonycfuentes"})
+
+(def email
+  {:display "anthony@anthonyfuentes.us"
+   :url "mailto:anthony@anthonyfuentes.us"})
+
+(def contact-info
+  {:email email
+   :linkedin linkedin
+   :github github})
+
+(def resume
+  {:name "Anthony Fuentes"
+   :contact-info contact-info})
