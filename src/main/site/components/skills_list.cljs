2ecf1970a007547271f778a8b5fb8f6da60c5f36
Anthony Fuentes <anthony@anthonyfuentes.us>
2020-08-26 16:49:18 -0700

    Refactor experience, skills lists

--- src/main/site/components/skills_list.cljs
+++ src/main/site/components/skills_list.cljs
@@ -1,8 +1,9 @@
 (ns site.components.skills-list
   (:require [site.components.skills-item :as si]))
 
+(defn render-items [{:keys [display] :as skill-area}]
+  ^{:key display} [si/render skill-area])
+
 (defn render [{:keys [skills]}]
   [:ul.skills-list
-   (map-indexed
-     (fn [i skill-area] ^{:key i} [si/render skill-area])
-     skills)])
+   (map render-items skills)])
