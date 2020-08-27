92e086b7a6f8f049bbb56e45460e1e92c1cd115a
Anthony Fuentes <anthony@anthonyfuentes.us>
2020-08-27 16:37:08 -0700

    Add education data

---
+++ src/main/site/data/education.cljs
@@ -0,0 +1,24 @@
+(ns site.data.education)
+
+(def foothill
+  {:organization "Foothill College"
+   :description "Java Programming"
+   :gpa "4.0"
+   :dates "Apr 2020 - Present"})
+
+(def purdue
+  {:organization "Purdue University"
+   :description "Mathematics for Computer Science"
+   :gpa "4.0"
+   :dates "Jan 2018 - Dec 2019"})
+
+(def ucsd
+  {:organization "UC San Diego"
+   :description "B.A. Political Science"
+   :gpa "3.74"
+   :dates "Sep 2008 - Dec 2012"})
+
+(def all
+  [foothill
+   purdue
+   ucsd])
