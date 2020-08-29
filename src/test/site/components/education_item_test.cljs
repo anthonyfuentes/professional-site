935d11ea50493b77c02fe3ea4de7479a09e56bb4
Anthony Fuentes <anthony@anthonyfuentes.us>
2020-08-29 06:58:12 -0700

    Test education-item#gpa-string

--- src/test/site/components/education_item_test.cljs
+++ src/test/site/components/education_item_test.cljs
@@ -1,10 +1,15 @@
 (ns site.components.education-item-test
   (:require [cljs.test :refer [deftest is]]
             [site.enzyme :as e :refer [shallow]]
-            [site.components.education-item :refer [render]]))
+            [site.components.education-item :as sut]))
 
 (deftest has-class-education-item
-  (-> (render {})
+  (-> (sut/render {})
       shallow
       (.hasClass "education-item")
+      is))
+
+(deftest gpa-string-generates-correct-string
+  (-> (sut/gpa-string "4.0")
+      (= "(4.0 GPA)")
       is))
