ee838a47556a79ed6552114f36e4e24cc541c8ed
Anthony Fuentes <anthony@anthonyfuentes.us>
2020-09-03 12:28:13 -0700

    Implement PDF download button

--- src/main/site/components/resume.cljs
+++ src/main/site/components/resume.cljs
@@ -2,11 +2,13 @@
   (:require [site.components.header :as head]
             [site.components.experience :as exp]
             [site.components.skills :as skl]
-            [site.components.education :as edu]))
+            [site.components.education :as edu]
+            [site.components.download :as dl]))
 
 (defn render [props]
   [:article.resume
    [head/render props]
    [exp/render props]
    [skl/render props]
-   [edu/render props]])
+   [edu/render props]
+   [dl/render]])
